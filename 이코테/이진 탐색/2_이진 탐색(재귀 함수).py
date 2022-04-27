baf7be523c53e229767b2db5aeeebb4783ae4d05
summerlight636 <summerlight@kakao.com>
2022-04-27 15:08:14 +0900

    이진 탐색 2_이진 탐색(재귀 함수): 원소가 존재하지 않는다고 나오는 오류

---
+++ 이코테/이진 탐색/2_이진 탐색(재귀 함수).py
@@ -0,0 +1,21 @@
+def binary_search(array, target, start, end):
+    # 재귀니까 종료조건 먼저(실패)
+    if start > end:
+        return None
+    # 성공
+    mid = (start + end) // 2
+    if target == array[mid]:
+        return mid
+    # 재귀
+    elif target < array[mid]:
+        binary_search(array, target, start, mid-1)
+    else:
+        binary_search(array, target, mid+1, end)
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않습니다")
+else:
+    print(result+1)
