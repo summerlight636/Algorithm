78fa2d1197f29f44013074ac7e2e64d353feef0a
summerlight636 <summerlight@kakao.com>
2022-04-27 11:55:18 +0900

    최단 경로 예제 1_간단한 다익스트라 2_개선된 다익스트라 3_플로이드 워셜
    최단 경로 실전 문제
    4_미래 도시
    5_전보

---
+++ 최단 경로/5_전보.py
@@ -0,0 +1,45 @@
+import heapq
+import sys
+
+#상수
+INF = int(1e9)
+
+#입력
+n, m, start = map(int, input().split())
+graph=[[] for i in range(n+1)]
+distance=[INF]*(n+1)
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+#실행
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance(now) < dist:
+            continue
+
+        for i in graph[now]:
+            cost = i[1] + dist
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+#출력
+count = 0
+max_distance = 0
+
+for d in distance:
+    if d != INF:
+        count += 1
+        max_distance = max(distance, d)
+
+print(count-1, end = ' ')
+print(max_distance)
+
