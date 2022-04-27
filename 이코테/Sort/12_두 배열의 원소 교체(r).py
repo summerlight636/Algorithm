ca9735b99b7840c713f5ba2bcd297911121d9a3d
summerlight636 <summerlight@kakao.com>
2022-04-27 14:28:57 +0900

    정렬 실전 문제 복습

---
+++ 이코테/Sort/12_두 배열의 원소 교체(r).py
@@ -0,0 +1,19 @@
+# 입력
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+#실행
+sum = 0
+a = sorted(a)
+b = sorted(b, reverse=True)
+
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+
+for i in range(n):
+    sum += a[i]
+
+#출력
+print(sum)
