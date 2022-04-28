02145bbe3e8d85875669084a982718ee79c77d10
summerlight636 <summerlight@kakao.com>
2022-04-28 13:13:30 +0900

    최단 경로 3_플로이드 워셜(r): - 갑자기 처음에 바꾼 값이 나중에도 영향을 주는 건 아닌가 하면서 헷갈리기 시작했다. 생각해보니 k-a-b 반복문이기 때문에 같은 graph[a][b]에 다른 k가 들어갈 일이 없어 중복 되게 대입될 일이 없었다. k가 가장 바깥에 있기 때문이다. (다음에 한 번 다시보자.)

---
+++ 이코테/최단 경로/3_플로이드 워셜(r).py
@@ -0,0 +1,28 @@
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+a = []
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range (1, n+1):
+        for b in range (1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][a])
+
+# 출력
+for i in range (n):
+    for j in range (n):
+        print(graph[i][j], end=' ')
+    print()
+
