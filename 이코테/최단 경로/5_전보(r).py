4793b2fe181bcb3750f7d87e7ccf3c6d27a2d919
summerlight636 <summerlight@kakao.com>
2022-04-28 14:15:04 +0900

    최단 경로 5_전보(r) 3 2 1 1 2 4 1 3 2 Traceback (most recent call last):   File "C:\Users\summe\Documents\AlgorithmStudy\이코테\최단 경로\5_전보(r).py", line 30, in <module>     dijkstra(start)   File "C:\Users\summe\Documents\AlgorithmStudy\이코테\최단 경로\5_전보(r).py", line 22, in dijkstra     if dist > distance[i]: UnboundLocalError: local variable 'i' referenced before assignment

---
+++ 이코테/최단 경로/5_전보(r).py
@@ -0,0 +1,42 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+#입력
+n, m, start = map(int, input().split())
+graph = [[] for i in range(n+1)]
+for i in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+distance = [INF] * (n+1)
+
+#실행
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[i]:
+            continue
+        for i in range(graph[now]):
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heqpq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+
+#출력
+count = 0
+max = 0
+for i in range(1, n+1):
+    if distance[i] != INF:
+        count += 1
+        if max < distance[i]:
+            max = distance[i]
+
+print(count, max)
