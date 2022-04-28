3d49f6a55b0470e859b18c9757ebabc03484847b
summerlight636 <summerlight@kakao.com>
2022-04-28 13:36:42 +0900

    최단 경로 3_플로이드 워셜(r): 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 - graph[a][b] 계산하는 본 함수 수정 0 4 8 6 3 0 7 9 5 9 0 4 7 11 2 0

--- 이코테/최단 경로/3_플로이드 워셜(r).py
+++ 이코테/최단 경로/3_플로이드 워셜(r).py
@@ -18,7 +18,7 @@ for _ in range(m):
 for k in range(1, n+1):
     for a in range (1, n+1):
         for b in range (1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][a])
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
 # 출력
 for i in range (1, n+1):
