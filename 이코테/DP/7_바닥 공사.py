c5e080bc85440c4753c1abcbd663bc5979a0da61
summerlight636 <summerlight@kakao.com>
2022-04-28 10:23:48 +0900

    DP 7_바닥 공사: 3 5 제대로 출력은 되지만, an을 출력한다는 의미에서 그냥 항 번호를 맞추는 것이 더 깔끔하지 않을까 해서 수정해보았다. 점화식은 변하지 않으니 간단히 수정할 수 있었다.

--- 이코테/DP/7_바닥 공사.py
+++ 이코테/DP/7_바닥 공사.py
@@ -2,13 +2,13 @@
 n = int(input())
 
 # 특수조건
-d = [0]*1000
-d[0] = 1
-d[1] = 3
+d = [0]*1001
+d[1] = 1
+d[2] = 3
 
 # 실행
-for i in range(2, n):
+for i in range(3, n+1):
     d[i] = (d[i-1] + 2*d[i-2]) % 796796
 
 # 출력
-print(d[n-1])
+print(d[n])
