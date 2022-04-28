5fade10313bc93e60c3c9cdb5f6b9fd301713a6c
summerlight636 <summerlight@kakao.com>
2022-04-28 09:38:58 +0900

    DP 5_1로 만들기: - input 26 output 13 (답: 26 -> 3) - d[i] = i -1 이라고 썼다. (전 항과의 관계를 이용해야 했었는데.) - min(d[i], ~) 으로 쓰는 게 훨씬 안정적이었을 것이다. 만약 실제로 작동하지 않는다고 하더라도, 최솟값을 골라야 하는 것은 변하지 않기 때문이다.

--- 이코테/DP/5_1로 만들기.py
+++ 이코테/DP/5_1로 만들기.py
@@ -6,9 +6,9 @@ x = int(input())
 d = [0]*30001
 
 for i in range(1, x+1):
-    d[i] = d[i - 1] + 1
+    d[i] = d[i - 1] + 1 #
     if i % 2 == 0:
-        d[i] = min(d[i], d[i//2] + 1)
+        d[i] = min(d[i], d[i//2] + 1) #
     if i % 3 == 0:
         d[i] = min(d[i], d[i//3] + 1)
     if i % 5 == 0:
