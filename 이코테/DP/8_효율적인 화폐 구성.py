2cca2febbc07bda088d91dc9cf0f6807199df692
summerlight636 <summerlight@kakao.com>
2022-04-28 10:54:50 +0900

    DP 8_효율적인 화폐 구성: - 중첩 for 문 작성에서부터 막혔다: 시작 지점을 무엇으로 잡아야할 지 부터 헷갈려서 시간이 엄청 오래 걸렸다. 점화식을 좀 더 명확하게 쓸 수 있도록 연습해야할 것 같다.

---
+++ 이코테/DP/8_효율적인 화폐 구성.py
@@ -0,0 +1,22 @@
+# 입력
+n, m = map(int, input().split())
+a = []
+for i in range(n):
+    a.append(int(input()))
+
+# 특수 조건
+d = [10001] * (m+1)
+d[0] = 0
+
+# 실행
+for i in range(n):
+    for j in range(a[i], m+1):
+        if d[j-a[i]] != 10001:
+            d[j] = min(d[j], d[j-a[i]] + 1)
+
+
+# 출력
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
