7e03c3cf3871a712dedaf285bbd2c7c8bc3516dc
summerlight636 <summerlight@kakao.com>
2022-05-04 18:52:18 +0900

    그리디 기출문제 5_볼링공 고르기: 5 3 1 3 2 3 2 6 - 값 틀림:

---
+++ 이코테 기출문제/그리디/5_볼링공 고르기.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+k = list(map(int, input().split()))
+k.sort()
+x = set(k)
+
+d = [0]*11
+
+for v in k:
+    d[v] += 1
+
+count = 0
+for i in x:
+    for j in range(i+1, 10):
+        if d[j] != 0:
+            count += d[j]
+
+print(count)
