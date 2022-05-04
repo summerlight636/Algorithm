b35f3b616bf752d3fee444387196884a70454b65
summerlight636 <summerlight@kakao.com>
2022-05-04 15:00:53 +0900

    DP 복습 8_효율적인 화폐 구성: 3 4 3 5 7 Traceback (most recent call last):   File "C:\Users\summe\Documents\AlgorithmStudy\이코테\DP\8_효율적인 화폐 구성(r).py", line 20, in <module>     d[i] = min(d[i], d[i-v] + 1) IndexError: list index out of range
    Process finished with exit code 1

---
+++ 이코테/DP/8_효율적인 화폐 구성(r).py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+INF = 10001
+
+#입력
+n, m = map(int, input().split())
+
+array = []
+for _ in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m+1)
+d[0] = 0
+
+for i in range(m+1):
+    for v in array:
+        if i % v == 0:
+            d[i] = min(d[i], i//v)
+
+        d[i] = min(d[i], d[i-v] + 1)
+
+print(d[m])
