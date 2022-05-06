f403a30c8b3eeb9db6cdd58ce2eb5e208dbbe1f4
summerlight636 <summerlight@kakao.com>
2022-05-06 10:38:08 +0900

    4 4 1 1 0 4 4 1 1 0 1 1 1 1 1 0 0 1 1 1 0 1 1 1 1 1 3 : global d 로 선언했던 것 때문에 오류가 발생했던 듯 하다. global d는 def solutions의 d에 영향을 주지 않기 때문에 turn_left가 실행되지 않았던 듯하다.

--- 이코테/Implementation/4_게임 개발(3).py
+++ 이코테/Implementation/4_게임 개발(3).py
@@ -6,44 +6,45 @@ for i in range(n):
     a = list(map(int, input().split()))
     graph.append(a)
 
-visited = [[0]*m for _ in range(n)]
-visited[x][y] = 1
+def turn_left(d):
+    result = (d-1) % 4
+    return result
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+def solutions(n, m, x, y, d, graph):
+    visited = [[0] * m for _ in range(n)]
+    visited[x][y] = 1
 
-def turn_left():
-    global d
-    d = (d-1) % 4
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
 
-turn_time = 0
-result = 1
-while True:
-    turn_left()
-    nx = x + dx[d]
-    ny = y + dy[d]
+    turn_time = 0
+    result = 1
+    while True:
+        d = turn_left(d)
+        nx = x + dx[d]
+        ny = y + dy[d]
 
-    if visited[nx][ny] == 0 and graph[nx][ny] == 0:
-        x = nx
-        y = ny
-        visited[x][y] = 1
-        result += 1
-        turn_time = 0
-        continue
-    else:
-        turn_time += 1
-
-    if turn_time == 4:
-        nx = x - dx[d]
-        ny = y - dy[d]
-
-        if graph[nx][ny] == 0:
+        if visited[nx][ny] == 0 and graph[nx][ny] == 0:
             x = nx
             y = ny
+            visited[x][y] = 1
+            result += 1
+            turn_time = 0
+            continue
         else:
-            break
-        turn_time = 0
+            turn_time += 1
 
-print(result)
+        if turn_time == 4:
+            nx = x - dx[d]
+            ny = y - dy[d]
 
+            if graph[nx][ny] == 0:
+                x = nx
+                y = ny
+            else:
+                break
+            turn_time = 0
 
+    print(result)
+
+solutions(n, m, x, y, d, graph)
