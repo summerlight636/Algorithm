ece237888deca27416c6aa17413a0876959aeaaa
summerlight636 <summerlight@kakao.com>
2022-05-06 10:29:33 +0900

    4 4 1 1 0 1 1 1 1 1 0 0 1 1 1 0 1 1 1 1 1 3
    Process finished with exit code 0
    : 함수 형태 였던 걸 바꿔주니 바로 정상 실행되었다. 왜지?

--- 이코테/Implementation/4_게임 개발(3).py
+++ 이코테/Implementation/4_게임 개발(3).py
@@ -6,37 +6,44 @@ for i in range(n):
     a = list(map(int, input().split()))
     graph.append(a)
 
+visited = [[0]*m for _ in range(n)]
+visited[x][y] = 1
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 def turn_left():
     global d
-    d = (d - 1) % 4
+    d = (d-1) % 4
 
-def solutions(x, y, d, graph):
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    turn_time = 0
-    graph[x][y] = 1
+turn_time = 0
+result = 1
+while True:
+    turn_left()
+    nx = x + dx[d]
+    ny = y + dy[d]
 
-    result = 0
-    while True:
-        if turn_time == 4:
-            nx = x - dx[d]
-            ny = y - dy[d]
-            if graph[nx][ny] == 1:
-                break
-
-        turn_left()
+    if visited[nx][ny] == 0 and graph[nx][ny] == 0:
+        x = nx
+        y = ny
+        visited[x][y] = 1
+        result += 1
+        turn_time = 0
+        continue
+    else:
         turn_time += 1
-        nx = x + dx[d]
-        ny = y + dy[d]
+
+    if turn_time == 4:
+        nx = x - dx[d]
+        ny = y - dy[d]
 
         if graph[nx][ny] == 0:
-
             x = nx
             y = ny
-            graph[x][y] = 1
-            result += 1
-            turn_time = 0
+        else:
+            break
+        turn_time = 0
 
-    print(result)
+print(result)
 
-solutions(x, y, d, graph)
+
