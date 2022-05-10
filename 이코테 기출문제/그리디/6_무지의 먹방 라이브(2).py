901f8184d13aafda9666024399e25a1822bd3165
summerlight636 <summerlight@kakao.com>
2022-05-10 09:54:50 +0900

    틀린 결과 : 큐 안의 값은 변화 하지 않는데, k값만 바꾸었기 때문에 문제 발생 => 해결

--- 이코테 기출문제/그리디/6_무지의 먹방 라이브(2).py
+++ 이코테 기출문제/그리디/6_무지의 먹방 라이브(2).py
@@ -2,7 +2,6 @@
 import heapq
 food_times = [3, 1, 2]
 k = 5
-
 def solution(food_times, k):
     l = len(food_times)
 
@@ -10,11 +9,15 @@ def solution(food_times, k):
     for i in range(l):
         heapq.heappush(q, (food_times[i], i + 1))
 
-    while q[0][0] * l <= k:
-        k -= heapq.heappop(q)[0] * l
+    sumv = 0
+    prev = 0
+    while sumv + ((q[0][0]-prev) * l) <= k:
+        now = heapq.heappop(q)[0]
+        sumv += (now-prev) * l
         l -= 1
+        prev = now
 
     result = sorted(q, key = lambda x:x[1])
-    return result[(k % l)][1]
+    return result[(k -sumv) % l][1]
 
 print(solution(food_times, k))
