b07f748521b404a21ab687e77d9d40b1817d8840
summerlight636 <summerlight@kakao.com>
2022-05-10 16:00:14 +0900

    왜 틀렸는지 모르겠다.

--- 이코테 기출문제/구현/11_뱀.py
+++ 이코테 기출문제/구현/11_뱀.py
@@ -1,18 +1,54 @@
 n = int(input())
-graph = [[0]*n for _ in rnage(n)]
-k = int(input())
+graph = [[0]*(n+1) for _ in range(n+1)]
 
-apple = []
+k = int(input())
 for i in range(k):
     n, m = map(int, input().split())
-    apple.append((x, y))
+    graph[n][m] = 1
 
 l = int(input())
-
 direction = []
 for i in range(l):
     x, c = input().split()
     direction.append((int(x), c))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]d
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
+
+x = 1
+y = 1
+d = 0
+time = 0
+graph[x][y] = 2
+q = [(1, 1)]
+while True:
+    time += 1
+    nx = x + dx[d]
+    ny = y + dy[d]
+    print(nx, ny)
+    print(graph[nx][ny])
+    if 1<= nx <= n and 1<= ny <= n and graph[nx][ny] != 2:
+        if graph[nx][ny] == 0:
+            a, b = q.pop(0)
+            graph[a][b] = 0
+        graph[nx][ny] = 2
+        q.append((nx, ny))
+        x, y = nx, ny
+        print(nx, ny)
+    else:
+        time += 1
+        x, y = nx, ny
+
+        break
+
+    for t, v in direction:
+        if time == t:
+            if v == 'L':
+                d = (d - 1) % 4
+                break
+            else:
+                d = (d + 1) % 4
+                break
+
+print(graph)
+print(time)
