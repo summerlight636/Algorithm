f3295ea9776317d951f7141c0f6a1f1669144ca3
summerlight636 <summerlight@kakao.com>
2022-05-14 18:11:35 +0900

    성공은 했지만, count = stages.count(i) 함수를 사용했다면 훨씬 간결해질 것이다.

---
+++ 이코테 기출문제/정렬/25_실패율.py
@@ -0,0 +1,25 @@
+def solution(N, stages):
+    answer = []
+
+    challenging = [0] * (N + 2)
+
+    for v in stages:
+        challenging[v] += 1
+
+    failure_rate = [0] * (N + 2)
+
+    result = []
+    for i in range(1, N + 1):
+        total = 0
+        for j in range(i, N + 2):
+            total += challenging[j]
+        if total == 0:
+            failure_rate[i] = 0
+        else:
+            failure_rate[i] = challenging[i] / total
+        result.append((i, failure_rate[i]))
+
+    result = sorted(result, key = lambda x:x[1], reverse=True)
+    for i in range(len(result)):
+        answer.append(result[i][0])
+    return answer
