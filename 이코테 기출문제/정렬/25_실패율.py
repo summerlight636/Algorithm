031bbb5752ad7d82914d7e89e6ed4484e0d656fa
summerlight636 <summerlight@kakao.com>
2022-05-14 18:21:41 +0900

    1) count = stages.count(i) 함수를 이용하여 간결하게 작성
    2) count 와 length의 상관관계를 이용하여 간결하게 작성
    
    3) answer = [i[0] for i in answer] 표현 기법 적용

--- 이코테 기출문제/정렬/25_실패율.py
+++ 이코테 기출문제/정렬/25_실패율.py
@@ -1,20 +1,20 @@
 def solution(N, stages):
     answer = []
+    length = len(stages)
 
-    failure_rate = [0] * (N + 2)
-
-    result = []
     for i in range(1, N + 1):
-        total = 0
-        for j in range(i, N + 2):
-            total += challenging[j]
-        if total == 0:
-            failure_rate[i] = 0
-        else:
-            failure_rate[i] = challenging[i] / total
-        result.append((i, failure_rate[i]))
+        count = stages.count(i)
 
-    result = sorted(result, key = lambda x:x[1], reverse=True)
-    for i in range(len(result)):
-        answer.append(result[i][0])
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        answer.append((i, fail))
+        length -= count
+
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+
+    answer = [i[0] for i in answer]
+
     return answer
