06affdb8987c488fb4fb6bbb9405cbe64e667add
summerlight636 <summerlight@kakao.com>
2022-07-12 12:12:36 +0900

    1시간10분/40분 - 버그 수정에 30분 이상 걸렸다. - 전역 변수를 초기화를 해 주지 않아 문제가 발생했던 것인데, 겉으로 보기에는 티가 나지 않아서 왜 작동이 안 되는지 알 수 없어서 애꿎은 곳만 계속 보면서 시간 낭비했다. def를 이용해서 알고리즘을 짤 때는 변수 설정을 잘 해주거나, 영향이 미치는 범위를 꼭 고려해주자.

---
+++ 이코테 기출문제/DFS/16_연구소(2).py
@@ -0,0 +1,68 @@
+# 임포트/상수
+from itertools import combinations
+INF = int(1e9)
+
+# 입력
+n, m = map(int, input().split())
+jido = []
+for i in range(n):
+    jido.append(list(map(int, input().split())))
+
+# 상수
+space = []
+virus_position = []
+for i in range(n):
+    for j in range(m):
+        if jido[i][j] == 0:
+            space.append((i, j))
+        elif jido[i][j] == 2:
+            virus_position.append((i, j))
+
+# 알고리즘
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def virus(x, y):
+    jido[x][y] = 2
+    visited[x][y] = True
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0<=nx and nx<=n-1 and 0<=ny and ny<=m-1:
+            if not visited[nx][ny]:
+                if jido[nx][ny] != 1:
+                    virus(nx, ny)
+
+def count_0(jido):
+    count = 0
+    for i in range(n):
+        for j in range(m):
+           if jido[i][j] == 0:
+                count += 1
+    return count
+
+def simulation():
+    result = 0
+    for walls in list(combinations(space, 3)):
+
+        global visited
+        visited = [[False] * m for _ in range(n)]
+
+        for a, b in walls:
+            jido[a][b] = 1
+
+        for x, y in virus_position:
+            virus(x, y)
+
+        result = max(result, count_0(jido))
+
+        for a, b in space:
+            jido[a][b] = 0
+
+    return result
+
+# 출력
+
+print(simulation())
