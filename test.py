14d251621df4fc0eaab05dd489a50aade70081dd
하영 <82437499+summerlight636@users.noreply.github.com>
2022-07-25 16:40:20 +0900

    test

--- test.py
+++ test.py
@@ -1,23 +1,9 @@
-# 임포트/상수
-from itertools import permutations
-from collections import deque
-
-# 알고리즘def solutions:
-perm = permutations(dist, len(dist))
-
-min_value = 8
-for p in perm:
-    p = deque(p)
-    count = 0
-    dist = 0
-    for w in weak:
-        if dist < w:
-            power = p.popleft()
-            count += 1
-            dist = w + power
-        else:
-            continue
-    min_value = min(min_value, count)
-
-# 출력
-print(min_value)
+print(-3//3)
+print(-3//4)
+print(-3//2)
+print(3//3)
+print(3//4)
+print(3//2)
+print(int(-3/2))
+print(int(-3//2))
+print(int(-1.5))
