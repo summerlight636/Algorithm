1bb1b249689eec47f4114d443e1bc00390077eb3
하영 <82437499+summerlight636@users.noreply.github.com>
2022-07-27 15:21:09 +0900

    60/60 #백준에서 런타임 에러(Recursion Error)발생 #Recursion Error는 아래와 같이 해결했지만, #import sys #sys.setrecursionlimit(10**6) #결국 시간초과가 되었다. #계속 확인해봤지만 왜 안되는지 모르겠다...

--- 이코테 기출문제/BFS/21_인구 이동(2).py
+++ 이코테 기출문제/BFS/21_인구 이동(2).py
@@ -9,7 +9,6 @@ for i in range(n):
     A.append(list(map(int, input().split())))
 
 # 상수
-INF = int(1e9)
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
@@ -17,6 +16,7 @@ dy = [0, 1, 0, -1]
 def dfs(x, y):
     global visited, t_num, t_space, t_list
     visited[x][y] = True
+    locked[x][y] = True
     t_num += A[x][y]
     t_space += 1
     t_list.append((x, y))
@@ -26,12 +26,13 @@ def dfs(x, y):
         ny = y + dy[i]
 
         if 0 <= nx <= n-1 and 0 <= ny <= n-1:
-            if not visited[nx][ny] and l <= abs(A[x][y]-A[nx][ny]) <= r:
+            if not visited[nx][ny] and not locked[nx][ny] and l <= abs(A[x][y]-A[nx][ny]) <= r:
                 dfs(nx, ny)
 
 cond = True
 count = 0
 visited = [[False]*n for _ in range(n)]
+locked = [[False]*n for _ in range(n)]
 while cond:
     for i in range(n):
         for j in range(n):
@@ -48,7 +49,10 @@ while cond:
                 t_avg = t_num // t_space
                 for x, y in t_list:
                     A[x][y] = t_avg
+
                 if t_space != 1:
+                    for a, b in t_list:
+                        locked[a][b] = False
                     cond = True
 
     if cond:
